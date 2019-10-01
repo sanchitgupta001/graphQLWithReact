041a1dad4c3205e8f96d6f4d88be58a4206e2585
sanchitgupta001 <sanchit.gupta@sprinklr.com>
2019-10-01 12:02:15 +0530

    Add basic graphql ecosystem

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Graph QL With React
-This repo consists of a simple application built using React and GraphQL
+
+This repo consists of a simple application built using React and GraphQL.
+
+Root Query: At the top level of every GraphQL server is a type that represents all of the possible entry points into the GraphQL API,
+it's often called the Root type or the Query type.
---
+++ prod/users/schema/schema.js
@@ -0,0 +1,128 @@
+/**
+ * Created by sanchitgupta001 on 08/09/2019
+ */
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull // for required validation
+} = require('graphql');
+const axios = require('axios');
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
+    // this is done in order to remove circular reference; graphQL will internally execute this as a function;
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(resp => resp.data); // axios returns response corresponding to data
+      }
+    }
+  })
+});
+
+const UserType = new GraphQLObjectType({
+  name: 'User', // type
+  fields: () => ({
+    // all properties which 'User' will have
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        console.log(parentValue);
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(resp => resp.data);
+      }
+    }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: {
+        // arguments required for the root query
+        id: {
+          type: GraphQLString
+        }
+      },
+      // resolve: used to find the data from the database
+      resolve(parentValue, args) {
+        // args: arguments with which query is called
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data); // axios returns response corresponding to data
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then(resp => resp.data); // axios returns response corresponding to data
+      }
+    }
+  }
+});
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, { firstName, age }) {
+        return axios
+          .post('http://localhost:3000/users', { firstName, age })
+          .then(res => res.data);
+      }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, { id }) {
+        return axios
+          .delete(`http://localhost:3000/users/${id}`)
+          .then(res => res.data);
+      }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .patch(`http://localhost:3000/users/${args.id}`, args) // 'id' will be ignored by graphql server
+          .then(res => res.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery, mutation });
